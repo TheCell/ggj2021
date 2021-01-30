b3e2c356f62aebf51136f78de9766f66d001d3b3
Echwemo <Echwemo@users.noreply.github.com>
2021-01-30 16:56:10 +0100

    Update FXSounds

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,50 +7,50 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private GameObject[] _itemSprites;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private AudioClip[] _FXSounds;
+    
+ 
 
     public void DiplayItemOne()
     {
         _itemSprites[0].SetActive(true);
+        _itemSprites[0].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemTwo()
     {
         _itemSprites[1].SetActive(true);
+        _itemSprites[1].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemThree()
     {
         _itemSprites[2].SetActive(true);
+        _itemSprites[2].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemFour()
     {
         _itemSprites[3].SetActive(true);
+        _itemSprites[3].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemFive()
     {
         _itemSprites[4].SetActive(true);
+        _itemSprites[4].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemSix()
     {
         _itemSprites[5].SetActive(true);
+        _itemSprites[5].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemSeven()
     {
         _itemSprites[6].SetActive(true);
+        _itemSprites[6].GetComponent<AudioSource>().Play();
     }
 }
