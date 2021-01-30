24fa225cb5cbe550577a818ffbe49509f2b34a70
TheCell <simon_hischier@hotmail.com>
2021-01-30 14:58:01 +0100

    added item types and image stuff

---
+++ Assets/BillBoard/ClueTypes.cs
@@ -0,0 +1,6 @@
+public enum ClueTypes
+{
+    Item = 1,
+    Text = 2,
+    Character = 3
+}
--- Assets/BillBoard/DragDropItem.cs
+++ Assets/BillBoard/DragDropItem.cs
@@ -3,42 +3,34 @@ using UnityEngine.InputSystem;
 
 public class DragDropItem : MonoBehaviour
 {
+    //public ClueTypes clueType = ClueTypes.Item;
+    public ItemTypes ItemType
+    {
+        get { return _itemType; }
+        set
+        {
+            _itemType = value;
+            SetImage();
+        }
+    }
+
+    [SerializeField]
+    private ItemTypes _itemType;
+    [SerializeField]
+    private ItemAndImage[] allItemAndImages;
+
     private PlayerInput playerInput;
     private InputActionMap inputActionMap;
     private bool clickPressed = false;
     private Transform objectTransformToDrag;
+    private SpriteRenderer spriteRenderer;
 
     public void Start()
     {
-        // I'm SURE this can be done MUCH better
-        playerInput = FindObjectOfType<PlayerInput>();
-        if (playerInput != null)
-        {
-            var actionMaps = playerInput.actions.actionMaps;
-            foreach (var map in actionMaps)
-            {
-                if (map.name.Equals("UI"))
-                {
-                    inputActionMap = map;
-                }
-            }
-        }
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
-        if (inputActionMap != null)
-        {
-            var actions = inputActionMap.actions;
-            foreach (var action in actions)
-            {
-                if (action.name.Equals("Click"))
-                {
-                    action.performed += OnClick;
-                }
-                else if (action.name.Equals("Point"))
-                {
-                    action.performed += OnDrag;
-                }
-            }
-        }
+        SetupInputAction();
+        SetImage();
     }
 
     public void OnClick(InputAction.CallbackContext context)
@@ -71,6 +63,56 @@ public class DragDropItem : MonoBehaviour
             var worldPos = Camera.main.ScreenToWorldPoint(mousePos);
             worldPos.z = objectTransformToDrag.position.z;
             objectTransformToDrag.position = worldPos;
+        }
+    }
+
+    private void SetImage()
+    {
+        if (spriteRenderer == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < allItemAndImages.Length; i++)
+        {
+            var itemAndImage = allItemAndImages[i];
+            if (itemAndImage.itemType == _itemType)
+            {
+                spriteRenderer.sprite = itemAndImage.sprite;
+            }
+        }
+    }
+
+    private void SetupInputAction()
+    {
+        // I'm SURE this can be done MUCH better
+        playerInput = FindObjectOfType<PlayerInput>();
+        if (playerInput != null)
+        {
+            var actionMaps = playerInput.actions.actionMaps;
+            foreach (var map in actionMaps)
+            {
+                if (map.name.Equals("UI"))
+                {
+                    inputActionMap = map;
+                }
+            }
+        }
+
+        if (inputActionMap != null)
+        {
+            var actions = inputActionMap.actions;
+            foreach (var action in actions)
+            {
+                if (action.name.Equals("Click"))
+                {
+                    action.performed += OnClick;
+                }
+                else if (action.name.Equals("Point"))
+                {
+                    action.performed += OnDrag;
+                }
+            }
         }
     }
 }
