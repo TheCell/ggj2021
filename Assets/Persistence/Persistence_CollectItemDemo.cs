de070554cb8f424c9920674ba96059d370ef3c87
TheCell <simon_hischier@hotmail.com>
2021-01-30 18:23:26 +0100

    singleton is hashset

--- Assets/Persistence/Persistence_CollectItemDemo.cs
+++ Assets/Persistence/Persistence_CollectItemDemo.cs
@@ -4,12 +4,12 @@ public class Persistence_CollectItemDemo : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.transform.name);
+        //Debug.Log(collision.transform.name);
         var itemDemo = collision.transform.GetComponent<Persistence_ItemDemo>();
         if (itemDemo != null)
         {
             PersistenceSingleton.Instance.AddNewItem(itemDemo.ItemType);
-            Debug.Log("added to persistence" + itemDemo.ItemType);
+            //Debug.Log("added to persistence" + itemDemo.ItemType);
             Destroy(collision.transform.gameObject);
         }
     }
