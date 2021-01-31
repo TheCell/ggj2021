1f0d9aec57cea9409d3d568fcd955331ff5b76fd
TheCell <simon_hischier@hotmail.com>
2021-01-31 14:59:03 +0100

    sounds working

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,9 +6,8 @@ public class Item : MonoBehaviour
     private ItemTypes itemType;
 
     [SerializeField]
-    private AudioClip[] _FXClip; 
+    private AudioClip _FXClip; 
 
-    // Start is called before the first frame update
     void Start()
     {
         if (PersistenceSingleton.Instance.FoundItems.Contains(itemType))
@@ -17,47 +16,14 @@ public class Item : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PickUp()
     {
         Player player = GameObject.Find("Detective").GetComponent<Player>();
         player.AddCollectible(itemType);
         PersistenceSingleton.Instance.AddNewItem(itemType);
 
+        AudioSource.PlayClipAtPoint(_FXClip, transform.position, 1.0f);
 
-        switch (itemType)
-        {
-            case ItemTypes.Item_CigaretteButt:
-                AudioSource.PlayClipAtPoint(_FXClip[0], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_IDCard:
-                AudioSource.PlayClipAtPoint(_FXClip[1], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_Invoice:
-                AudioSource.PlayClipAtPoint(_FXClip[2], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_Jewelry:
-                AudioSource.PlayClipAtPoint(_FXClip[3], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_KODrops:
-                AudioSource.PlayClipAtPoint(_FXClip[4], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_Rosin:
-                AudioSource.PlayClipAtPoint(_FXClip[5], transform.position, 1.0f);
-                break;
-            case ItemTypes.Item_Warderobenumber:
-                AudioSource.PlayClipAtPoint(_FXClip[6], transform.position, 1.0f);
-                break;
-            default:
-                Debug.Log("Default value");
-                break;
-        }
-        // here we need AudioSource PickUp jingle
         Destroy(this.gameObject);
     }
 
