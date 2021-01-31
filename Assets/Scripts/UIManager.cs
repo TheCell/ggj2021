a7604add1f2e25378a128eb7617e9f5ed94e3677
pierrefranklin <pierre.franklin@gmail.com>
2021-01-31 03:02:42 +0100

    Try to fix null audio thing

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -15,42 +15,35 @@ public class UIManager : MonoBehaviour
     public void DiplayItemOne()
     {
         _itemSprites[0].SetActive(true);
-        _itemSprites[0].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemTwo()
     {
         _itemSprites[1].SetActive(true);
-        _itemSprites[1].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemThree()
     {
         _itemSprites[2].SetActive(true);
-        _itemSprites[2].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemFour()
     {
         _itemSprites[3].SetActive(true);
-        _itemSprites[3].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemFive()
     {
         _itemSprites[4].SetActive(true);
-        _itemSprites[4].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemSix()
     {
         _itemSprites[5].SetActive(true);
-        _itemSprites[5].GetComponent<AudioSource>().Play();
     }
 
     public void DisplayItemSeven()
     {
         _itemSprites[6].SetActive(true);
-        _itemSprites[6].GetComponent<AudioSource>().Play();
     }
 }
